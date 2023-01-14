bf49d6140ef6b84e5be5a6a34fc4f82be95852d1
0ddbird <giacomo.llinares@gmail.com>
2023-01-14 19:05:18 +0100

    FEAT : multiline result for angle to cos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const decimals = parseInt(document.querySelector('#input_decimals').value)
+
 const cosForm = document.querySelector('#cosinus_form')
 const sinForm = document.querySelector('#sinus_form')
 const tanForm = document.querySelector('#tangente_form')
@@ -11,9 +11,12 @@ tanForm.addEventListener('submit', handleTanFormSubmit)
 angForm.addEventListener('submit', handleAngFormSubmit)
 
 const toDeg = (radA) => radA / (Math.PI / 180)
+const getDecimals = () => parseInt(document.querySelector('#input_decimals').value)
+
 
 function handleCosFormSubmit(e) {
     e.preventDefault()
+    const decimals = getDecimals()
     const input = parseFloat(document.querySelector('#input_cosinus').value)
     const rad = Math.acos(input)
     const deg = toDeg(rad)
@@ -23,6 +26,7 @@ function handleCosFormSubmit(e) {
 
 function handleSinFormSubmit(e) {
     e.preventDefault()
+    const decimals = getDecimals()
     const input = parseFloat(document.querySelector('#input_sinus').value)
     const rad = Math.asin(input)
     const deg = toDeg(rad)
@@ -32,6 +36,7 @@ function handleSinFormSubmit(e) {
 
 function handleTanFormSubmit(e) {
     e.preventDefault()
+    const decimals = getDecimals()
     const input = parseFloat(document.querySelector('#input_tangente').value)
     const rad = Math.atan(input)
     const deg = toDeg(rad)
@@ -42,12 +47,38 @@ function handleTanFormSubmit(e) {
 
 function handleAngFormSubmit(e) {
     e.preventDefault()
+    const decimals = getDecimals()
     const input = parseFloat(document.querySelector('#input_angle').value)
     const sin = Math.sin(input).toFixed(decimals)
     const cos = Math.cos(input).toFixed(decimals)
     const tan = Math.tan(input).toFixed(decimals)
-    const result = `Cosinus : ${cos}, Sinus : ${sin}, Tangente : ${tan}`
-    appendResult('Angle', `${input}°`, result)
+    const result = [`Cosinus : ${cos}`, `Sinus : ${sin}`, `Tangente : ${tan}`]
+    appendAngleResult('Angle', `${input}°`, result)
+}
+
+function appendAngleResult(cat, inpt, resArr) {
+    const row = document.createElement('div')
+    const catSpan = document.createElement('span')
+    const inputSpan = document.createElement('span')
+    const resultP = document.createElement('p')
+
+    row.classList.add('row')
+    catSpan.classList.add('category')
+    inputSpan.classList.add('input_value')
+    resultP.classList.add('res_value')
+    catSpan.innerText = cat
+    inputSpan.innerText = inpt
+
+    resArr.forEach(res => {
+        const resSpan = document.createElement('span')
+        resSpan.innerText = res
+        resultP.appendChild(resSpan)
+    })
+
+    row.appendChild(catSpan)
+    row.appendChild(inputSpan)
+    row.appendChild(resultP)
+    resultContainer.appendChild(row)
 }
 
 function appendResult(cat, inpt, res) {
