d69e0d7f13c574ea39b9a5c02c1589d025722337
0ddbird <giacomo.llinares@gmail.com>
2023-01-14 18:52:49 +0100

    FEAT : added arcsin, arctan and angle to cos/sin/tan

--- index.js
+++ index.js
@@ -1,27 +1,71 @@
-const form = document.querySelector('#cosinus_form')
+const decimals = parseInt(document.querySelector('#input_decimals').value)
+const cosForm = document.querySelector('#cosinus_form')
+const sinForm = document.querySelector('#sinus_form')
+const tanForm = document.querySelector('#tangente_form')
+const angForm = document.querySelector('#angle_form')
 const resultContainer = document.querySelector('#result_container')
-form.addEventListener('submit', handleFormSubmit)
 
+cosForm.addEventListener('submit', handleCosFormSubmit)
+sinForm.addEventListener('submit', handleSinFormSubmit)
+tanForm.addEventListener('submit', handleTanFormSubmit)
+angForm.addEventListener('submit', handleAngFormSubmit)
 
-function handleFormSubmit(e) {
+const toDeg = (radA) => radA / (Math.PI / 180)
+
+function handleCosFormSubmit(e) {
     e.preventDefault()
-    const decimals = parseInt(document.querySelector('#input_decimals').value)
-    const input = document.querySelector('#input_cosinus')
+    const input = parseFloat(document.querySelector('#input_cosinus').value)
+    const rad = Math.acos(input)
+    const deg = toDeg(rad)
+    const result = `${deg.toFixed(decimals)}°`
+    appendResult('Cos > Angle (deg)', input.toString(), result)
+}
 
-    const value = parseFloat(input.value)
-    const rad = Math.acos(value)
-    const deg = rad / (Math.PI / 180)
+function handleSinFormSubmit(e) {
+    e.preventDefault()
+    const input = parseFloat(document.querySelector('#input_sinus').value)
+    const rad = Math.asin(input)
+    const deg = toDeg(rad)
+    const result = `${deg.toFixed(decimals)}°`
+    appendResult('Sin > Angle (deg)', input.toString(), result)
+}
 
+function handleTanFormSubmit(e) {
+    e.preventDefault()
+    const input = parseFloat(document.querySelector('#input_tangente').value)
+    const rad = Math.atan(input)
+    const deg = toDeg(rad)
+    const result = `${deg.toFixed(decimals)}°`
+    appendResult('Tan > Angle (deg)', input.toString(), result)
+
+}
+
+function handleAngFormSubmit(e) {
+    e.preventDefault()
+    const input = parseFloat(document.querySelector('#input_angle').value)
+    const sin = Math.sin(input).toFixed(decimals)
+    const cos = Math.cos(input).toFixed(decimals)
+    const tan = Math.tan(input).toFixed(decimals)
+    const result = `Cosinus : ${cos}, Sinus : ${sin}, Tangente : ${tan}`
+    appendResult('Angle', `${input}°`, result)
+}
+
+function appendResult(cat, inpt, res) {
     const row = document.createElement('div')
-    const cosSpan = document.createElement('span')
-    const angleSpan = document.createElement('span')
+    const catSpan = document.createElement('span')
+    const inputSpan = document.createElement('span')
+    const resultSpan = document.createElement('span')
 
     row.classList.add('row')
-    cosSpan.classList.add('cos_value')
-    angleSpan.classList.add('res_value')
-    cosSpan.innerText = value.toString()
-    angleSpan.innerText = `${deg.toFixed(decimals)}°`
-    row.appendChild(cosSpan)
-    row.appendChild(angleSpan)
+    catSpan.classList.add('category')
+    inputSpan.classList.add('input_value')
+    resultSpan.classList.add('res_value')
+    catSpan.innerText = cat
+    inputSpan.innerText = inpt
+    resultSpan.innerText = res
+
+    row.appendChild(catSpan)
+    row.appendChild(inputSpan)
+    row.appendChild(resultSpan)
     resultContainer.appendChild(row)
 }
